c095c139d6cfd89f36ffa993b6103d6a528710f7
Андрей Илюшин <andreialexandrovich@gmail.com>
2019-01-09 00:57:57 +0300

    initial files add

---
+++ ColdCallsTracker/ColdCallsTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ColdCallsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
