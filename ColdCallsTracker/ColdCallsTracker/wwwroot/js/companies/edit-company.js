e9796c7f53a75fbcc4ed86c94dba25fefb28b189
ilushin <ilushin@corteos.ru>
2019-04-10 00:25:45 +0300

    Копировать в буфер

--- ColdCallsTracker/ColdCallsTracker/wwwroot/js/companies/edit-company.js
+++ ColdCallsTracker/ColdCallsTracker/wwwroot/js/companies/edit-company.js
@@ -1,4 +1,37 @@
-﻿$(function () {
+﻿function fallbackCopyTextToClipboard(text) {
+	var textArea = document.createElement("textarea");
+	textArea.value = text;
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
+
+	try {
+		var successful = document.execCommand('copy');
+		var msg = successful ? 'successful' : 'unsuccessful';
+		console.log('Fallback: Copying text command was ' + msg);
+	} catch (err) {
+		console.error('Fallback: Oops, unable to copy', err);
+	}
+
+	document.body.removeChild(textArea);
+}
+function copyTextToClipboard(text) {
+	if (!navigator.clipboard) {
+		fallbackCopyTextToClipboard(text);
+		return;
+	}
+	navigator.clipboard.writeText(text).then(function () {
+		console.log('Async: Copying to clipboard was successful!');
+	}, function (err) {
+		console.error('Async: Could not copy text: ', err);
+	});
+}
+
+
+
+$(function () {
+
+
 
     Vue.directive('mask',
         {
@@ -55,6 +88,10 @@
                 window.editPhoneModal.open();
                 window.editPhoneModal.isComplete = true;
             },
+            copyPhone(item) {
+                let number = this.entity.Phones.filter(x => x.Id == editCompany.selectedPhoneId)[0].Number;
+                copyTextToClipboard(number);
+            },
             async removePhone(item) {
                 let result = confirm("Хотите удалить телефон, все записи связанные с телефоном будут удалены!");
                 if (result == true) {
@@ -87,7 +124,7 @@
 
             let vm = this;
 
-            
+
 
             window.onPhoneChange = function () {
                 let entity = window.editPhoneModal.entity;
